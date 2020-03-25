268c29ec2b93ea59639a41e5c6f37475be193214
PayOT <payot.developer@gmail.com>
2020-03-25 16:32:24 +0900

    inital commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import VueNumeral from 'vue-numeral-filter';
+import VueMoment from 'vue-moment';
+import '@/assets/css/style.scss';
+
+Vue.use(Vuetify);
+Vue.use(VueNumeral);
+Vue.use(VueMoment);
+
+export default new Vuetify({});
